7f684d7d7035847006c976b1c4d25ca2772222bd
philippgie <philipp.giersfeld@gmail.com>
2023-03-10 01:17:22 +0200

    was working beofre changes with vectors

--- ppc/is/is4/is.cc
+++ ppc/is/is4/is.cc
@@ -16,7 +16,7 @@ std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 void printTime() {
   end = std::chrono::steady_clock::now();
   std::cout << "Time difference = "
-            << std::chrono::duration_cast<std::chrono::milliseconds>(end -
+            << std::chrono::duration_cast<std::chrono::microseconds>(end -
                                                                      begin)
                    .count()
             << "[µs]" << std::endl;
@@ -25,6 +25,9 @@ void printTime() {
 
 typedef double double4_t __attribute__((vector_size(4 * sizeof(double))));
 
+constexpr double infty = std::numeric_limits<double>::infinity();
+
+constexpr double4_t d4inf = {infty, infty, infty, infty};
 constexpr double4_t d4zero = {0.0, 0.0, 0.0, 0.0};
 // constexpr int P = 8;
 
@@ -61,18 +64,26 @@ This is the function you need to implement. Quick reference:
 Result segment(int ny, int nx, const float *data) {
   Result result{0, 0, 0, 0, {0, 0, 0}, {0, 0, 0}};
 
-  double *sums = new double[3 * nx * ny];
+  double4_t *sums = double4_alloc(nx * ny);
+  double4_t *data_4d = double4_alloc(nx * ny);
   // double* inner = new double(3 * nx * ny);
-
+  // #pragma omp parallel for
   for (int y = 0; y < ny; y++) {
-    double accumulator[3] = {0, 0, 0};
     for (int x = 0; x < nx; x++) {
       for (int c = 0; c < 3; c++) {
-        accumulator[c] += data[c + 3 * x + 3 * nx * y];
-        sums[c + 3 * x + 3 * nx * y] =
-            accumulator[c] + (y > 0 ? sums[c + 3 * x + 3 * nx * (y - 1)] : 0);
-        // std::cout << "Sum: " << sums[c + 3 * x + 3 * nx * y] << std::endl;
+        data_4d[x + nx * y][c] = data[c + 3 * x + 3 * nx * y];
       }
+      data_4d[x + nx * y][3] = 0.0;
+    }
+  }
+
+  for (int y = 0; y < ny; y++) {
+    double4_t accumulator = d4zero;
+    for (int x = 0; x < nx; x++) {
+      accumulator += data_4d[x + nx * y];
+      sums[x + nx * y] =
+          accumulator + (y > 0 ? sums[x + nx * (y - 1)] : d4zero);
+      // std::cout << "Sum: " << sums[c + 3 * x + 3 * nx * y] << std::endl;
     }
   }
   printTime();
@@ -87,57 +98,47 @@ Result segment(int ny, int nx, const float *data) {
       for (int y1 = y0 + 1; y1 <= ny; y1++) {
         for (int x0 = 0; x0 < nx; x0++) {
           for (int x1 = x0 + 1; x1 <= nx; x1++) {
-            double inner_avg[3] = {0.0, 0.0, 0.0};
-            double outer_avg[3] = {0.0, 0.0, 0.0};
-            for (int c = 0; c < 3; c++) {
-              if (x0 == 0 && y0 == 0) {
-                inner_avg[c] = sums[c + 3 * (x1 - 1) + 3 * nx * (y1 - 1)];
-              } else if (y0 == 0) {
-                inner_avg[c] = (sums[c + 3 * (x1 - 1) + 3 * nx * (y1 - 1)] -
-                                sums[c + 3 * (x0 - 1) + 3 * nx * (y1 - 1)]);
-              } else if (x0 == 0) {
-                inner_avg[c] = (sums[c + 3 * (x1 - 1) + 3 * nx * (y1 - 1)] -
-                                sums[c + 3 * (x1 - 1) + 3 * nx * (y0 - 1)]);
-              } else {
-                inner_avg[c] = (sums[c + 3 * (x0 - 1) + 3 * nx * (y0 - 1)] -
-                                sums[c + 3 * (x1 - 1) + 3 * nx * (y0 - 1)] -
-                                sums[c + 3 * (x0 - 1) + 3 * nx * (y1 - 1)] +
-                                sums[c + 3 * (x1 - 1) + 3 * nx * (y1 - 1)]);
-              }
-              outer_avg[c] =
-                  (sums[c + 3 * (nx - 1) + 3 * nx * (ny - 1)] - inner_avg[c]) /
-                  (nx * ny - (x1 - x0) * (y1 - y0));
-              inner_avg[c] /= (x1 - x0) * (y1 - y0);
-              // std::cout << "Average " << c << ": " << inner_avg[c] <<
-              // std::endl;
+            double4_t inner_avg;
+            double4_t outer_avg;
+            if (x0 == 0 && y0 == 0) {
+              inner_avg = sums[(x1 - 1) + nx * (y1 - 1)];
+            } else if (y0 == 0) {
+              inner_avg = (sums[(x1 - 1) + nx * (y1 - 1)] -
+                           sums[(x0 - 1) + nx * (y1 - 1)]);
+            } else if (x0 == 0) {
+              inner_avg = (sums[(x1 - 1) + nx * (y1 - 1)] -
+                           sums[(x1 - 1) + nx * (y0 - 1)]);
+            } else {
+              inner_avg = (sums[(x0 - 1) + nx * (y0 - 1)] -
+                           sums[(x1 - 1) + nx * (y0 - 1)] -
+                           sums[(x0 - 1) + nx * (y1 - 1)] +
+                           sums[(x1 - 1) + nx * (y1 - 1)]);
             }
-            double inner_error[3] = {0.0, 0.0, 0.0};
-            double outer_error[3] = {0.0, 0.0, 0.0};
+            outer_avg = (sums[(nx - 1) + nx * (ny - 1)] - inner_avg) /
+                        (nx * ny - (x1 - x0) * (y1 - y0));
+            inner_avg /= (x1 - x0) * (y1 - y0);
+            inner_avg *= inner_avg;
+            outer_avg *= outer_avg;
+            double4_t inner_error = d4zero;
+            double4_t outer_error = d4zero;
             for (int x = 0; x < nx; x++) {
               for (int y = 0; y < ny; y++) {
                 if ((x0 <= x) && (x < x1) && (y0 <= y) && (y < y1)) {
-                  for (int c = 0; c < 3; c++) {
-                    double t = data[c + 3 * x + 3 * nx * y] - inner_avg[c];
-                    inner_error[c] += t * t;
-                  }
+                  double4_t t = data_4d[x + nx * y] - inner_avg;
+                  inner_error += t * t;
                 } else {
-                  for (int c = 0; c < 3; c++) {
-                    double t = data[c + 3 * x + 3 * nx * y] - outer_avg[c];
-                    outer_error[c] += t * t;
-                  }
+                  double4_t t = data_4d[x + nx * y] - outer_avg;
+                  outer_error += t * t;
                 }
               }
             }
-            double seg_error = inner_error[0] + inner_error[1] +
-                               inner_error[2] + outer_error[0] +
-                               outer_error[1] + outer_error[2];
+            double seg_error =
+                reduce_double4(inner_error) + reduce_double4(outer_error);
+
             // std::cout << "Minimum error: " << min_error << std::endl;
             // std::cout << "Segment error: " << seg_error << std::endl;
             if (seg_error < min_thread_error) {
               min_thread_error = seg_error;
-              double seg_error = inner_error[0] + inner_error[1] +
-                                 inner_error[2] + outer_error[0] +
-                                 outer_error[1] + outer_error[2];
               thread_result.y1 = y1;
               thread_result.x0 = x0;
               thread_result.x1 = x1;
@@ -151,22 +152,23 @@ Result segment(int ny, int nx, const float *data) {
           }
         }
       }
+    }
 #pragma omp critical
-      if (min_thread_error < min_error) {
-        min_error = min_thread_error;
-        result.x0 = thread_result.x0;
-        result.x1 = thread_result.x1;
-        result.y0 = thread_result.y0;
-        result.y1 = thread_result.y1;
-        for (int c = 0; c < 3; c++) {
-          result.inner[c] = thread_result.inner[c];
-          result.outer[c] = thread_result.outer[c];
-        }
+    if (min_thread_error < min_error) {
+      min_error = min_thread_error;
+      result.x0 = thread_result.x0;
+      result.x1 = thread_result.x1;
+      result.y0 = thread_result.y0;
+      result.y1 = thread_result.y1;
+      for (int c = 0; c < 3; c++) {
+        result.inner[c] = thread_result.inner[c];
+        result.outer[c] = thread_result.outer[c];
       }
     }
   }
   printTime();
 
-  delete[] sums;
+  std::free(data_4d);
+  std::free(sums);
   return result;
 }
